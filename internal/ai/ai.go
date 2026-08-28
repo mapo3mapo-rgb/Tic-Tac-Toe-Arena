78ea92fb84c049d9b1c086aaa01218109d06c3e4
mapo3mapo-rgb <mapo3mapo@gmail.com>
2026-08-28 23:19:40 +0500

    Update ai.go

--- internal/ai/ai.go
+++ internal/ai/ai.go
@@ -1,28 +1,28 @@
 package ai
 
-import "github.com/khiyuazha-cell/Raid-3-Tic-Tac-Toe-Arena/internal/board"
+import "github.com/mapo3mapo-rgb/Tic-Tac-Toe-Arena/internal/board"
 
 func GetMove(b board.Board) int {
 	size := b.Size
 	cells := b.Cells
 
-	// Правило 1 — победить
+	// Правило 1 победить
 	if move := tryComplete(b, "O"); move != -1 {
 		return move
 	}
 
-	// Правило 2 — заблокировать
+	// Правило 2 заблокировать
 	if move := tryComplete(b, "X"); move != -1 {
 		return move
 	}
 
-	// Правило 3 — центр
+	// Правило 3 центр
 	center := (size * size) / 2
 	if cells[center] == "" {
 		return center + 1
 	}
 
-	// Правило 4 — угол
+	// Правило 4 угол
 	corners := []int{0, size - 1, size * (size - 1), size*size - 1}
 	for _, i := range corners {
 		if cells[i] == "" {
@@ -30,7 +30,7 @@ func GetMove(b board.Board) int {
 		}
 	}
 
-	// Правило 5 — сторона
+	// Правило 5 сторона
 	for i, cell := range cells {
 		if cell == "" {
 			return i + 1
