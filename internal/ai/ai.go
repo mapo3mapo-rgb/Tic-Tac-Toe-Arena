6cf75cb207964c9eb13b9371b4b18810962f7e18
ddddddaaaaa769 <davidabramov19@gmail.com>
2026-08-30 21:57:31 +0500

    Modify GetMove to return move type and reason

--- internal/ai/ai.go
+++ internal/ai/ai.go
@@ -2,42 +2,42 @@ package ai
 
 import "github.com/mapo3mapo-rgb/Tic-Tac-Toe-Arena/internal/board"
 
-func GetMove(b board.Board) int {
+func GetMove(b board.Board) (int, string) {
 	size := b.Size
 	cells := b.Cells
 
 	// Правило 1 победить
 	if move := tryComplete(b, "O"); move != -1 {
-		return move
+		return move, "win"
 	}
 
 	// Правило 2 заблокировать
 	if move := tryComplete(b, "X"); move != -1 {
-		return move
+		return move, "block"
 	}
 
 	// Правило 3 центр
 	center := (size * size) / 2
 	if cells[center] == "" {
-		return center + 1
+		return center + 1, "center"
 	}
 
 	// Правило 4 угол
 	corners := []int{0, size - 1, size * (size - 1), size*size - 1}
 	for _, i := range corners {
 		if cells[i] == "" {
-			return i + 1
+			return i + 1, "corner"
 		}
 	}
 
 	// Правило 5 сторона
 	for i, cell := range cells {
 		if cell == "" {
-			return i + 1
+			return i + 1, "side"
 		}
 	}
 
-	return -1
+	return -1, ""
 }
 
 func tryComplete(b board.Board, mark string) int {
